0629f5ec796437fb386882016b2bc2b530a78eba
Bryan Kennedy <bryankennedy@users.noreply.github.com>
2025-12-29 21:14:32 -0600

    feat: add zsh and starship configuration

---
+++ zsh/.zshrc
@@ -0,0 +1,53 @@
+# Homebrew Setup
+if [[ -f /opt/homebrew/bin/brew ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+# Antidote
+source /opt/homebrew/opt/antidote/share/antidote/antidote.zsh
+
+# Initialize Antidote
+# Initialize Antidote
+# Ensure zsh-completions is loaded early
+fpath=(${ZDOTDIR:-$HOME}/.antidote/plugins/zsh-users/zsh-completions/src $fpath)
+
+if [[ ! -f ${ZDOTDIR:-$HOME}/.zsh_plugins.zsh ]]; then
+  antidote bundle < ${ZDOTDIR:-$HOME}/.zsh_plugins.txt > ${ZDOTDIR:-$HOME}/.zsh_plugins.zsh
+fi
+source ${ZDOTDIR:-$HOME}/.zsh_plugins.zsh
+autoload -Uz compinit && compinit
+
+# Case-insensitive completion (cd r -> Resources)
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+
+
+# Starship
+eval "$(starship init zsh)"
+
+
+# Modern Tools
+if command -v zoxide > /dev/null; then
+  eval "$(zoxide init zsh --cmd j)"
+fi
+
+
+if command -v fnm > /dev/null; then
+  eval "$(fnm env --use-on-cd)"
+fi
+
+# Eza Colors (Crispy Theme)
+# da=dark_gray(timestamp), ur=green(user_read), uw=red(user_write), ux=blue(user_exec)
+# gr=green(group_read), gw=red(group_write), gx=blue(group_exec)
+# tr=green(other_read), tw=red(other_write), tx=blue(other_exec)
+# sn=orange(size), sb=orange(size_unit), uu=light_gray(user), gu=light_gray(group)
+export EZA_COLORS="da=37:ur=32:uw=31:ux=34:gr=32:gw=31:gx=34:tr=32:tw=31:tx=34:sn=33:sb=33:uu=37:gu=37"
+
+
+
+# Aliases
+[ -f "$HOME/dotfiles/zsh/aliases.zsh" ] && source "$HOME/dotfiles/zsh/aliases.zsh"
+
+# User configuration
+export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+export PATH="$HOME/go/bin:$PATH"
+
---
+++ zsh/aliases.zsh
@@ -0,0 +1,139 @@
+# Aliases and Functions
+
+# Shorthand
+alias c='clear'
+alias g='grep'
+alias xx='exit'
+
+# Ask before doing anything dangerous
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+
+# Readable path
+alias path='echo -e ${PATH//:/\\n}'
+
+# -------------------------------------------------------------------
+# Listing
+# -------------------------------------------------------------------
+
+# List normal files
+alias l='eza -lh --icons=auto --group-directories-first --time-style=long-iso'
+# List everything, including hidden files
+alias ll='eza -lAh --icons=auto --group-directories-first --time-style=long-iso'
+# List for wildcard searches without all those subdir files
+alias lw='eza -lAhd --icons=auto --group-directories-first --time-style=long-iso'
+# List everything, by reverse date
+alias lld='eza -lAh --icons=auto --group-directories-first --time-style=long-iso --sort=modified'
+
+# -------------------------------------------------------------------
+# Searching
+# -------------------------------------------------------------------
+
+findin() {
+  find . -exec grep -q "$1" '{}' \; -print
+}
+
+# -------------------------------------------------------------------
+# Navigation
+# -------------------------------------------------------------------
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+
+
+# Compress the cd, ls -l series of commands.
+cl() {
+   if [ $# = 0 ]; then
+      cd && l
+   else
+      cd "$*" && l
+   fi
+}
+# Alias for common miss-type
+alias lc='cl'
+
+# Compress the mkdir > cd into it series of commands
+mc() {
+  mkdir -p "$*" && cd "$*" && pwd
+}
+
+# -------------------------------------------------------------------
+# Extracting
+# -------------------------------------------------------------------
+
+extract() {
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)   tar xvjf $1    ;;
+            *.tar.gz)    tar xvzf $1    ;;
+            *.tgz)       tar xvzf $1    ;;
+            *.bz2)       bunzip2 $1     ;;
+            *.rar)       unrar x $1     ;;
+            *.gz)        gunzip $1      ;;
+            *.tar)       tar xvf $1     ;;
+            *.tbz2)      tar xvjf $1    ;;
+            *.zip)       unzip $1       ;;
+            *.Z)         uncompress $1  ;;
+            *.7z)        7z x $1        ;;
+            *)           echo "'$1' cannot be extracted via >extract<" ;;
+        esac
+    else
+        echo "'$1' is not a valid file"
+    fi
+}
+
+# -------------------------------------------------------------------
+# macOS Specific Tools
+# -------------------------------------------------------------------
+
+if [[ $(uname) == "Darwin" ]]; then
+  # Replicate the tree function on macOS if not installed
+  if ! command -v tree &> /dev/null; then
+    alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
+  fi
+
+  # Quicker smaller top
+  alias topp='top -ocpu -R -F -s 2 -n30'
+
+  # Copy current directory to clipboard
+  alias cpwd='pwd|xargs echo -n|pbcopy'
+
+  # Dump man pages to Preview
+  pman() {
+    man -t "${1}" | open -f -a /Applications/Preview.app/
+  }
+
+  # Vim - prefer standard vim or mvim if available
+  if command -v mvim &> /dev/null; then
+    alias vi='mvim'
+  else
+    alias vi='vim'
+  fi
+else
+  # Linux / VSCode / Etc
+  alias vi='vim'
+fi
+
+# -------------------------------------------------------------------
+# Git
+# -------------------------------------------------------------------
+alias gs='git status -s -b'
+alias gc='git commit -v'
+alias ga='git add'
+alias gaa='git add -A'
+alias gco='git checkout'
+alias gd='git diff'
+alias gps='git push'
+alias gpsm='git push origin master'
+alias gpsd='git push origin develop'
+alias gpl='git pull'
+alias gplm='git pull origin master'
+alias gpld='git pull origin develop'
+
+# -------------------------------------------------------------------
+# Reload Zsh
+# -------------------------------------------------------------------
+alias reload='source ${ZDOTDIR:-$HOME}/.zshrc'
