13c8bdce04116fec18e8af1474eac7034547cd66
bkennedy <bkennedy@smm.org>
2026-04-11 08:17:26 -0500

    feat(nvim): add neovim config as stow package
    Initial neovim configuration with init.lua.

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,8 @@
+-- Neovide GUI settings
+if vim.g.neovide then
+  -- Disable cursor animation
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_scroll_animation_length = 0
+  vim.g.neovide_position_animation_length = 0
+end
